44197287f42ef7236a58ce20786d54e62312df56
deoyani <deoyani.pics@gmail.com>
2019-08-07 15:34:34 -0400

    Added authentication related changes to the restapi

---
+++ java/customization-api/src/main/java/gov/nist/oar/custom/customizationapi/config/SAMLConfig/SecurityConstant.java
@@ -0,0 +1,11 @@
+package gov.nist.oar.custom.customizationapi.config.SAMLConfig;
+
+
+public class SecurityConstant {
+
+    public static final String JWT_SECRET = "yeWAgVDfb$!MFn@MCJVN7uqkznHbDLR#";
+
+    private SecurityConstant(){}
+
+
+}
